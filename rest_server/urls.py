38c29ccfbb247fbc6d8f763c5e846b72879130e2
sekjohn <sujohn478@gmail.com>
2020-09-11 11:53:40 +0900

    전체 코드

--- rest_server/urls.py
+++ rest_server/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import DefaultRouter
+from teststart  import views
 
+
+router = DefaultRouter()
+router.register('post',views.PostViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls'),
+    path('',include(router.url)),
 ]
--- teststart/views.py
+++ teststart/views.py
@@ -1,25 +1,9 @@
 from .models import Post
 from .Serializers import Postserializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.http import Http404
-from rest_framework import status
-
+from rest_framework.viewsets import ModelViewSet
 
 # 클래식 베잇스 뷰(CBV)
-class Postlist(APIView):
-    
-    
-    def get(self, request, format= None):
-        postquery = Post.objects.all()
-        serializer = Postserializer(postquery, many=True)
-        return Response(serializer.data)
-
-        
-    def post(self,request, format=None):
-        serializer = Postserializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_calss = Postserializer    
 
