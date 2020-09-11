162be6f5c73a545ddc520b2a9604be7f2adb5830
sekjohn <sujohn478@gmail.com>
2020-09-11 11:37:40 +0900

    코드

--- rest_server/urls.py
+++ rest_server/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from teststart import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snippets/', views.Postlist.as_view()),
+    path('api-auth', include('rest_framework.urls'),
 ]
--- teststart/models.py
+++ teststart/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):  
-    postid  = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=10)
-    body = models.CharField(max_length=50)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
