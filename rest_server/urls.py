0a2ac7ee79e98e88f4f8a227c3ffb58c2e0df2c7
sekjohn <sujohn478@gmail.com>
2020-09-14 13:39:26 +0900

    전체 코드

--- rest_server/urls.py
+++ rest_server/urls.py
@@ -8,5 +8,6 @@ router = DefaultRouter()
 router.register('post',views.PostViewSet)
 
 urlpatterns = [
+    path('public/', views.PublicPostListAPIView.as_view()),
     path('',include(router.urls)),
 ]
--- teststart/views.py
+++ teststart/views.py
@@ -1,14 +1,16 @@
 from .models import Post
 from .Serializers import Postserializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics 
+
+
+class PublicPostListAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
+
 
 # 클래식 베잇스 뷰(CBV)
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = Postserializer
 
-    def dispatch(self, request, *args, **kwargs):
-        print("request.body : ", request.body)
-        print("request.body : ", request.body)
-        return super().dispatch(request, *args, **kwargs)
-    
