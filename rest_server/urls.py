f61c6dafea32e62a4e8a830e2ad299f5e04688f0
sekjohn <sujohn478@gmail.com>
2020-09-18 11:21:57 +0900

    전체 코드

--- rest_server/urls.py
+++ rest_server/urls.py
@@ -8,4 +8,5 @@ from teststart  import views
 urlpatterns = [
     path('post/', views.PostListAPIView.as_view()),
     path('post/<int:pk>', views.PostDetailAPIView.as_view())
+    path('accounts/', include('accounts.urls')),
 ]
